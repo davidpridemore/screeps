72e4e86c8bc9664ab36353842dbdd024b8021fe7
David Pridemore <david@pridemo.re>
2021-06-23 17:31:02 -0500

    Added working memory flags to empty storage before harvesting again

--- role.harvester.js
+++ role.harvester.js
@@ -2,13 +2,18 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0) {
+        if(creep.store.energy == 0) { // energy is empty
+            creep.memory.harvesting = true;
+        } else if (creep.store.getFreeCapacity() == 0) { // capacity is full
+            creep.memory.harvesting = false;
+        } 
+        
+        if(creep.memory.harvesting == true) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#FFFF00'}});
             }
-        }
-        else {
+        } else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
@@ -16,8 +21,14 @@ var roleHarvester = {
                     }
             });
             if(targets.length > 0) {
+                // Transfer to spawn or extensions with free energy capacity
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#0000ff'}});
+                }
+            } else {
+                // Transfer energy to controller
+                if(creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#0000ff'}});
                 }
             }
         }
